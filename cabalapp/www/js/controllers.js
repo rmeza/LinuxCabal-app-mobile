4ffd90c7d50cfea1b7f770003c28bc52b47d14f5
Ricardo Meza <ing.ricardomeza@gmail.com>
2015-04-24 12:08:30 -0500

    first commit

---
+++ cabalapp/www/js/controllers.js
@@ -0,0 +1,22 @@
+﻿/**
+ *
+
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+*/
+
+'use strict';
+
+// optional controllers
+function HomeCtrl($scope, $http) {
+}
+
+function ProjectCtrl($scope, $http) {
+}
+
+function PrivacyCtrl($scope, $http, $timeout) {
+}
+
+function AboutCtrl($scope, $http, $timeout) {
+}
